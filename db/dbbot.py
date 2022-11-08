df6add84dd23986c23668785edaba55379e378d1
tel1ce <zamorskijaleksej88@gmail.com>
2022-11-08 20:39:45 +0300

    little upgrade

--- db/dbbot.py
+++ db/dbbot.py
@@ -7,11 +7,12 @@ cur = conn.cursor()
 
 cur.execute("""CREATE TABLE IF NOT EXISTS music(
             musicid INT PRIMARY KEY,
-            musicname TEXT);
+            musicname TEXT,
+            musichash TEXT );
 """)
 conn.commit()
 
 
-def musicdb_add(musicid: int, musicname: str):
-    cur.execute('INSERT INTO music(?,?); VALUES (?, ?)')
+def musicdb_add(musicid: int, musicname: str, musichash: str):
+    cur.execute('INSERT INTO music(?, ?, ?); VALUES (?, ?, ?)')
     conn.commit()
