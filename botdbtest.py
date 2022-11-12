9df79c04762dc587aa8d5da8f468f5ae422b1f87
tel1ce <zamorskijaleksej88@gmail.com>
2022-11-12 22:09:21 +0300

    a little cosmetic changes

--- botdbtest.py
+++ botdbtest.py
@@ -1,24 +1,19 @@
-import telebot
-import time
-import schedule
+import telebot, time, schedule
 
-token = '5747352743:AAFNu1qNIA1i-Gaft71cwZqRTAz34JnxZGg'
-bot = telebot.TeleBot(token)
+
+bot = telebot.TeleBot('5747352743:AAFNu1qNIA1i-Gaft71cwZqRTAz34JnxZGg')
 k = 1
 
 
 @bot.message_handler(content_types=['audio'])
 def handle_docs_photo(message):
-
     try:
         file_info = bot.get_file(message.audio.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
-
         src ='C:/' + file_info.file_path
         with open(src, 'wb') as new_file:
            new_file.write(downloaded_file)
         bot.reply_to(message, "Аудио добавлено")
-
     except Exception as e:
         bot.reply_to(message, e)
 
@@ -28,6 +23,7 @@ def send_message():
 
 
 schedule.every().day.at("21:59").do(send_message)
+
 
 while True:
     schedule.run_pending()
